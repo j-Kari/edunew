fd69e66bad54c145e1257d4e45fecbb7287468f4
Eliza Wang <elizawang0728@gmail.com>
2023-06-29 10:13:06 -0400

    Initial Commit

---
+++ edu/eduApp.swift
@@ -0,0 +1,17 @@
+//
+//  eduApp.swift
+//  edu
+//
+//  Created by Eliza Wang on 2023-06-29.
+//
+
+import SwiftUI
+
+@main
+struct eduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
